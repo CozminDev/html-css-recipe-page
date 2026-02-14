3fec81a4fca2883b9ca00067f3eb0f9708599473
Cozmin <cozmindeveloper@yahoo.com>
2026-02-14 12:44:07 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'outfit': ['Outfit'],
+        'young-serif': ['YoungSerif']
+      }
+    }
+  }
+}
